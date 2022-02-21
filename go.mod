f3ef661227a167168100e0699984eda7a902f6c3
William Gunawan <wilgun123@gmail.com>
2022-02-21 15:58:37 +0700

    update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/wilgun/go-say-hello
+module github.com/wilgun/go-say-hello/v2
 
 go 1.17
--- say_hello.go
+++ say_hello.go
@@ -1,7 +1,7 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello from module"
+func SayHello(name string) string {
+	return "Hello " + name + " from module"
 }
 
 func SayHello2() string {
